330f33d496447953b73f1cf3b89e837c7fdcb122
CuppingCare <9272warb@gmail.com>
2025-01-20 17:42:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    if (email === "" || password === "") {
+        alert("Please fill in both fields");
+    } else {
+        console.log("Email:", email);
+        console.log("Password:", password);
+        alert("Login successful!");
+    }
+});
